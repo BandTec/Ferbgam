ec37a204af35ef18da4393be8cc004af26a4e7f4
Fernandosantos1 <fernando.santos@bandtec.com>
2021-04-01 10:41:48 -0300

    att simulador

--- Sprint 2/Site Institucional/Simulador/JS/main.js
+++ Sprint 2/Site Institucional/Simulador/JS/main.js
@@ -6,10 +6,11 @@ function calculate() {
     var pack = Number(se_pack.value);
     var pack_percent = pack / 100;
     var eco = (employ * payment) * pack_percent;
-
-
+    var price_meters = (meters * 100) * pack_percent;
 
     div_javascript.style.display = 'block';
-    result_eco.innerHTML += `${eco}`;
-    result_price.innerHTML = `${eco}`;
+    result_name.innerHTML = `${name}`;
+    result_eco.innerHTML = `${eco}`;
+    result_price.innerHTML = `${price_meters}`;
+    result_porcent.innerHTML = `${pack}`
 }
