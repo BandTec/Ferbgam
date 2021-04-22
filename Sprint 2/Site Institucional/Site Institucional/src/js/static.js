b8928c95bdd3033e866cee699fd066d607f00a87
Manocchio <guruu.leste@gmail.com>
2021-04-22 19:23:06 -0300

    Mudando display de grid para flex

--- Sprint 2/Site Institucional/Site Institucional/src/js/static.js
+++ Sprint 2/Site Institucional/Site Institucional/src/js/static.js
@@ -1,44 +1,44 @@
-let contextLuminosity = document.querySelector('#chartLuminosity').getContext('2d');
+// let contextLuminosity = document.querySelector('#chartLuminosity').getContext('2d');
 
-let config = {
-    type: 'bar',
-    data: {
-        labels: ['Dia 1', 'Dia 2', 'Dia 3', 'Dia 3', 'Dia 4', 'Dia 5'],
-        datasets: [{
-            label: 'Lux',
-            data: [12, 19, 3, 5, 5, 10],
-            backgroundColor: [
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-            ],
-            borderColor: [
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-                'rgba(24,150,117)',
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        legend: {
-            position: "top",
-            align: "start"
-        },
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-};
+// let config = {
+//     type: 'bar',
+//     data: {
+//         labels: ['Dia 1', 'Dia 2', 'Dia 3', 'Dia 3', 'Dia 4', 'Dia 5'],
+//         datasets: [{
+//             label: 'Lux',
+//             data: [12, 19, 3, 5, 5, 10],
+//             backgroundColor: [
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//             ],
+//             borderColor: [
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//                 'rgba(24,150,117)',
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         legend: {
+//             position: "top",
+//             align: "start"
+//         },
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// };
 
 
 
-let chartLuminosity = new Chart(contextLuminosity, config);
+// let chartLuminosity = new Chart(contextLuminosity, config);
