875b7de6b43fc40dac121984677a84833d31d61f
Manocchio <guruu.leste@gmail.com>
2021-04-22 22:40:58 -0300

    mudando algumas coisas no gráfico

--- Sprint 2/Site Institucional/Site Institucional/src/js/static.js
+++ Sprint 2/Site Institucional/Site Institucional/src/js/static.js
@@ -3,18 +3,18 @@ let contextLuminosity = document.querySelector('#chartLuminosity').getContext('2
 let config = {
     type: 'bar',
     data: {
-        labels: ['20:00', '19:00', '18:00', '17:00', '16:00', '15:00', '14:00','13:00'],
+        labels: ['20:00', '19:00', '18:00', '17:00', '16:00', '15:00', '14:00', '13:00'],
         datasets: [{
             label: 'Lux',
             data: [1200, 500, 750, 984, 1000, 475, 1600, 800],
             backgroundColor: [
-                '#FF2501',
-                '#FFBB33',
+                '#d50606',
+                '#548235',
                 '#548235',
                 '#FF8800',
-                '#FF2501',
-                '#FFFF25',
-                '#FF2501',
+                '#d50606',
+                '#FF8800',
+                '#d50606',
                 '#548235',
             ],
             borderWidth: 1
@@ -45,19 +45,19 @@ let contextTemperature = document.querySelector('#chartTemperature').getContext(
 let configTemp = {
     type: 'bar',
     data: {
-        labels: ['20:00', '19:00', '18:00', '17:00', '16:00', '15:00', '14:00','13:00'],
+        labels: ['20:00', '19:00', '18:00', '17:00', '16:00', '15:00', '14:00', '13:00'],
         datasets: [{
             label: 'Cº',
-            data: [30, 19, 23, 26, 15, 28, 20, 54],
+            data: [30, 19, 23, 26, 15, 28, 20, 40],
             backgroundColor: [
-                '#FF2501',
+                '#FF8800',
+                '#FFBB33',
+                '#548235',
+                '#FF8800',
+                '#FFFF25',
                 '#FF8800',
                 '#548235',
-                '#548235',
-                '#FFFF25',
-                '#548235',
-                '#548235',
-                '#FF2501',
+                '#d50606',
             ],
             borderWidth: 1
         }]
@@ -87,18 +87,18 @@ let contextHumidity = document.querySelector('#chartHumidity').getContext('2d');
 let configHumi = {
     type: 'bar',
     data: {
-        labels: ['20:00', '19:00', '18:00', '17:00', '16:00', '15:00', '14:00','13:00'],
+        labels: ['20:00', '19:00', '18:00', '17:00', '16:00', '15:00', '14:00', '13:00'],
         datasets: [{
             label: '%',
             data: [10, 30, 98, 85, 20, 54, 77, 5],
             backgroundColor: [
                 '#FFFF25',
-                '#548235',
                 '#FFBB33',
-                '#FF2501',
+                '#d50606',
+                '#d50606',
+                '#FFFF25',
                 '#548235',
-                '#548235',
-                '#FF8800',
+                '#d50606',
                 '#FFFF25',
 
             ],
